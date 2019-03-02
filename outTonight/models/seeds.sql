a10012889964953f95b373d65b28f4932891b5c0
Christina Lavingia <43803376+christinalavingia@users.noreply.github.com>
2019-03-01 17:01:40 -0800

    Update seeds.sql
    Added seed data scheduled during presentation weekend for a timely demo.

--- outTonight/models/seeds.sql
+++ outTonight/models/seeds.sql
@@ -1,5 +1,35 @@
 USE eventsDb;
 
 INSERT INTO `events` (name, type, date, time, location, description, cost) 
-    VALUES ("Coachella 2019", "Festival", "17-19th April", "12pm", "INDIO, CA", "Music Festival", "$500");
+    VALUES ("LA Clippers vs. OKC Thunder", "Sporting Event", "Friday, March 8", "7:30 PM", "STAPLES Center", "Watch the Los Angeles Clippers Watch the Los Angeles Clippers play an exciting home game as they faceoff against the Oklahoma City Thunder.", "$20");
+   
+INSERT INTO `events` (name, type, date, time, location, description, cost)
+    VALUES (VALUES ("Musink Tattoo and Car Convention", "Convention", "Friday, March 8", "12:00 PM", "OC Fair and Event Center", "This convention is an all-ages convention running from Friday, March 8 to Sunday, March 10.", "$79");
+ 
+INSERT INTO `events` (name, type, date, time, location, description, cost) 
+    VALUES ("America's Funniest Home Videos", "Television", "Saturday, March 9", "1:00 PM", "ABC Manhattan Beach Studios", "Join the studio audience watching hysterical videos and funny commentary by host Afonso Ribeiro.", "Free");
+   
+INSERT INTO `events` (name, type, date, time, location, description, cost)
+    VALUES (VALUES ("OC Wine Fest 2019", "Festival", "Saturday, March 9", "12:00 PM", "San Juan Capistrano", "OC Wine Fest offers wine tasting, wine seminars, art vendors, and gourmet food at A Stone’s Throw Winery.", "$175");
+            
+INSERT INTO `events` (name, type, date, time, location, description, cost)
+    VALUES (VALUES ("Festival of Colors", "Festival", "Saturday, March 9", "11:00 AM", "South El Monte Whittier Narrows Park", "Holi: Festival of Colors sells brightly colored cornstarch for you to throw at each other and promises interactive dance, live mantra bands, DJs, yoga teachers, cuisine, free hugs, and lotsa love.", "Free");
+
+INSERT INTO `events` (name, type, date, time, location, description, cost)
+    VALUES (VALUES ("Kite Festival", "Festival", "Sunday, March 10", "11:00 AM", "Redondo Beach", "All-ages Festival of the Kite features a gigantic 25-foot kite, a dance performance, live classic rock, and prizes for the best handmade kite, best ground display, highest-flying kite, youngest flyer, and youngest at heart at Redondo Beach Pier.", "Free");
+
+INSERT INTO `events` (name, type, date, time, location, description, cost)
+    VALUES (VALUES ("Craig Robinson", "Comedy", "Saturday, March 9", "7:00 PM", "Brea Improv", "Schedule some laughs with actor and comedian of The Office fame, Craig Robinson!", "$30");
+            
+INSERT INTO `events` (name, type, date, time, location, description, cost)
+    VALUES (VALUES ("The Ralph Report with Ralph Garman", "Comedy", "Saturday, March 9", "10:30 PM", "Hollywood Improv (The Main Room)", "Join The Ralph Report podcast crew: Ralph Garman, Vice Host Eddie Pence, UK Correspondant Steve Ashton and Kari Garman for a live recording of THE RALPH REPORT.", "$20");
+              
+INSERT INTO `events` (name, type, date, time, location, description, cost)
+    VALUES (VALUES ("At the Improv: Darrell Hammond, Jimmy Shin and more TBA!", "Comedy", "Sunday, March 10", "6:30 PM", "Hollywood Improv (The Main Room)", "Join these legends of SNL and more for a night of laughs, political and social commentary, and more.", "$15");
+    
+INSERT INTO `events` (name, type, date, time, location, description, cost)
+    VALUES (VALUES ("Justin Timberlake", "Live Music", "Sunday, March 10", "7:00 PM", "STAPLES Center", "Timberlake is currently scheduled to perform 25 shows throughout North America, commencing with a performance in Toronto in March and wrapping up with a concert in Memphis, Tenn. on May 30.", "$120");
+    
+INSERT INTO `events` (name, type, date, time, location, description, cost)
+    VALUES (VALUES ("L.A. Opera - The Clemency of Titus", "Theater", "Sunday, March 10", "2:00 PM", "Out of Mozart's many operas, this one stands out – a rare treat with all of the drama and music you'd expect, plus an added dose of treachery and togas.", "$60");
     
