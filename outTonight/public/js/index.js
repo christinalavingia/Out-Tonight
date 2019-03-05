d1defae767450568bd583ccda48bd7794565e200
xkeekyx <xkeyanna@yahoo.com>
2019-03-04 19:57:53 -0800

    updates

--- outTonight/public/js/index.js
+++ outTonight/public/js/index.js
@@ -1,22 +1,31 @@
 // Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+// var $exampleText = $("#example-text");
+// var $exampleDescription = $("#example-description");
+var submitVendor = $("#submitNewEvent");
+// var $exampleList = $("#example-list");
+var searchBtn = $("#userSearch");
+
+var eventName = $("#eventName");
+var eventType = $("#eventType");
+var eventDate = $("#eventDate");
+var eventTime = $("#eventTime");
+var eventLocation = $("#eventLocation");
+var eventDescription = $("#eventDescription");
+var eventCost = $("#eventCost");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveEvent: function (event) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
       },
       type: "POST",
       url: "api/create",
-      data: JSON.stringify(example)
+      data: JSON.stringify(event)
     });
   },
-  getExamples: function() {
+  getEvent: function () {
     return $.ajax({
       url: "api/search",
       type: "GET"
@@ -25,74 +34,95 @@ var API = {
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
+// var refreshEvent = function() {
+//   API.getEvent().then(function(data) {
+//     var $examples = data.map(function(example) {
+//       var $a = $("<a>")
+//         .text(example.text)
+//         .attr("href", "/example/" + example.id);
 
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
+//       var $li = $("<li>")
+//         .attr({
+//           class: "list-group-item",
+//           "data-id": example.id
+//         })
+//         .append($a);
 
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
+//       var $button = $("<button>")
+//         .addClass("btn btn-danger float-right delete")
+//         .text("ｘ");
 
-      $li.append($button);
+//       $li.append($button);
 
-      return $li;
-    });
+//       return $li;
+//     });
 
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
+//     $exampleList.empty();
+//     $exampleList.append($examples);
+//   });
+// };
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmit = function (event) {
   event.preventDefault();
 
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
+  var eventListing = {
+    name: eventName.val().trim(),
+    type: eventType.val(),
+    date: eventDate.val(),
+    time: eventTime.val().trim(),
+    description: eventDescription.val().trim(),
+    location: eventLocation.val().trim(),
+    cost: eventCost.val().trim()
   };
+  console.log(eventName.val());
+  console.log(eventDescription.val());
+  console.log(eventType.val());
+  console.log(eventDate.val());
+  console.log(eventTime.val());
+  console.log(eventCost.val());
+  console.log(eventLocation.val());
 
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
+
+  if (eventName.val() === "" || eventDescription.val() === "" || eventDate.val() === ""
+    || eventTime.val() === "" || eventLocation.val() === "" || eventCost.val() === "" || eventType.val() === "") {
+    alert("You must fill out every field!");
+  } else {
+    API.saveEvent(eventListing).then(function () {
+      // refreshEvent();
+      alert("Event created successfully!");
+
+      return;
+    });
   }
 
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
+  eventName.val("");
+  eventType.val("Select an Option");
+  eventDate.val("");
+  eventTime.val("");
+  eventDescription.val("");
+  eventLocation.val("");
+  eventCost.val("");
 };
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
+// var handleDeleteBtnClick = function() {
+//   var idToDelete = $(this)
+//     .parent()
+//     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
+//   API.deleteExample(idToDelete).then(function() {
+//     refreshExamples();
+//   });
+// };
 
 // Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+submitVendor.on("click", handleFormSubmit);
+// $exampleList.on("click", ".delete", handleDeleteBtnClick);
+// searchBtn.on("click", )
 
-
-$(function () {
-  $("#datetimepicker1").datetimepicker();
-});
+// $(function () {
+//   $("#datetimepicker1").datetimepicker();
+// });
